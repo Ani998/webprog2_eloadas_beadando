1af40b7a740c76962121a9122f0cf0ea6a954587
Ani998 <trinyik.aniko@gmail.com>
2025-11-23 14:36:04 +0100

    regisztáció és login javítása

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -31,7 +31,8 @@ class AuthenticatedSessionController extends Controller
         $request->authenticate();
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+     return redirect()->intended('/');
+
     }
 
     /**
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,15 +13,19 @@
 <!-- NAVBAR -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
     <div class="container">
+
+        <!-- LOGO -->
         <a class="navbar-brand fw-bold" href="{{ url('/') }}">
             🎬 MoziDB
         </a>
 
+        <!-- MOBILE MENU BUTTON -->
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarMenu">
             <span class="navbar-toggler-icon"></span>
         </button>
 
+        <!-- MENU -->
         <div class="collapse navbar-collapse" id="navbarMenu">
             <ul class="navbar-nav ms-auto">
 
@@ -30,79 +34,80 @@
                     <a class="nav-link" href="{{ url('/') }}">Főoldal</a>
                 </li>
 
-                <!-- Filmek listája (előadások) -->
+                <!-- Filmek listája -->
                 <li class="nav-item">
                     <a class="nav-link" href="{{ url('/eloadasok') }}">Filmek listája</a>
                 </li>
 
-                <!-- CRUD – Filmek -->
+                <!-- CRUD -->
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('filmek.index') }}">CRUD </a>
+                    <a class="nav-link" href="{{ route('filmek.index') }}">CRUD</a>
                 </li>
 
                 <!-- Diagram -->
-<li class="nav-item">
-    <a class="nav-link" href="{{ route('diagram.index') }}">Diagram</a>
-</li>
-
-            
-                <!-- Kapcsolat -->
-<li class="nav-item">
-    <a class="nav-link" href="{{ url('/kapcsolat') }}">Kapcsolat</a>
-</li>
-
-@auth
-    <!-- Üzenetek (bejelentkezett felhasználóknak) -->
-    <li class="nav-item">
-        <a class="nav-link" href="{{ route('uzenetek.index') }}">Üzenetek</a>
-    </li>
-
-    @if(Auth::user()->role === 'admin')
-        <!-- Admin menü (csak adminoknak) -->
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('admin.index') }}">Admin</a>
-        </li>
-    @endif
-@endauth
-
-                 <!-- Autentikáció (dinamikus) -->
-    @guest
-        <!-- Ha nincs bejelentkezve -->
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('login') }}">Bejelentkezés</a>
-        </li>
-
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('register') }}">Regisztráció</a>
-        </li>
-    @else
-        <!-- Ha be van jelentkezve -->
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button"
-               data-bs-toggle="dropdown">
-                {{ Auth::user()->name }}
-            </a>
-
-            <ul class="dropdown-menu dropdown-menu-end">
-                <li>
-                    <form method="POST" action="{{ route('logout') }}">
-                        @csrf
-                        <button class="dropdown-item">Kijelentkezés</button>
-                    </form>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('diagram.index') }}">Diagram</a>
                 </li>
-            </ul>
-        </li>
-    @endguest
 
-</ul>
+                <!-- Kapcsolat -->
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/kapcsolat') }}">Kapcsolat</a>
+                </li>
+
+                @auth
+                    <!-- Üzenetek -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('uzenetek.index') }}">Üzenetek</a>
+                    </li>
+
+                    @if(Auth::user()->role === 'admin')
+                        <!-- Admin -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('admin.index') }}">Admin</a>
+                        </li>
+                    @endif
+                @endauth
+
+                <!-- LOGIN / REGISTER vagy USER DROPDOWN -->
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">Bejelentkezés</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">Regisztráció</a>
+                    </li>
+                @else
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown"
+                           role="button" data-bs-toggle="dropdown">
+                            {{ Auth::user()->name }}
+                        </a>
+
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li>
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+                                    <button class="dropdown-item">Kijelentkezés</button>
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                @endguest
+
+            </ul>
         </div>
+
     </div>
 </nav>
 
-<!-- Tartalom -->
+<!-- OLDAL TARTALOM -->
 <div class="container mt-4">
     @yield('content')
 </div>
 
 <!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
